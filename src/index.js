b3cfd98ff71ac8603d09d748fbd07960b8006e11
John Louie Binas <jlouie.binas@neworientalclub88.com>
2019-11-08 12:54:44 +0800

    Minor patch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-export default {
-	install: function(Vue) {
-		Vue.component("b-scaler", VueScale)
-	},
+import VueScale from "@/VueScale.vue";
+
+// Install function executed by Vue.use()
+function install(Vue, options) {
+  if (install.installed) return;
+  install.installed = true;
+
+  const components = {
+    scale: VueScale
+  };
+
+  // Declare all components when options is not set or array is empty
+  // Or when the user explicitely specify it
+  for (let component in components) {
+    if (
+      !options ||
+      !options.components ||
+      options.components.length === 0 ||
+      options.components.includes(component)
+    ) {
+      Vue.component("b-" + component, components[component]);
+    }
+  }
 }
+// Create module definition for Vue.use()
+const VueScale = {
+  install
+};
+
+// To auto-install when vue is found
+/* global window global */
+let GlobalVue = null;
+
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(VueScale);
+}
+
+// To allow use as module (npm/webpack/etc.) export components
+export default blitzy;
